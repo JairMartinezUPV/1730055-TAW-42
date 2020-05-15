c6c4906fd2f16d09c882948654091798adc89828
JairMartinezUPV <36892931+JairMartinezUPV@users.noreply.github.com>
2020-05-15 08:37:16 -0500

    Update crud.php

--- mvc/models/crud.php
+++ mvc/models/crud.php
@@ -59,6 +59,28 @@ class Datos extends Conexion
 	//modelo actuializar usuario
 	public function actuializarUsuarioModel($datosModel, $tabla){
 		$stmt = Conexion::conectar()->prepare("UPDATE $tabla SET usuario =:usuario, password = :password, email = :email WHERE id = :id");
+		$stmt->bindParam(":usuario", $datosModel["usuario"], PDO::PARAM_STR);
+		$stmt->bindParam(":password", $datosModel["password"], PDO::PARAM_STR);
+		$stmt->bindParam(":email", $datosModel["email"], PDO::PARAM_STR);
+		$stmt->bindParam(":id", $datosModel["id"], PDO::PARAM_STR);
+
+		if ($stmt->execute()) {
+			return "success";
+		}else{
+			return "error";
+		}
+	}
+
+	//modelo borrar usuario
+	public function borrarUsuarioModel($datosModel, $tabla){
+		$stmt = Conexion::conectar()->prepare("DELETE FROM $tabla WHERE id = :id");
+		$stmt->bindParam(":id", $datosModel, PDO::PARAM_INT);
+		if ($stmt->execute()) {
+			return "success";
+		}else{
+			return "error";
+		}
+		$stmt->close();
 	}
 }
 
