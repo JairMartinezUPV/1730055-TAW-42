9ba98ffe861addab5ae220ea20a34893289d52ba
JairMartinezUPV <36892931+JairMartinezUPV@users.noreply.github.com>
2020-05-15 08:39:56 -0500

    updates controller crud

--- mvc/controllers/controller.php
+++ mvc/controllers/controller.php
@@ -1,73 +1,109 @@
-<?php  
-class MvcController{
-	#llamadda a la plantilla
-	public function pagina(){
-		include "views/template.php";
-	}
-
-	//enlaces
-	public function enlacesPaginasController(){
-		$if (isset($_GET['action'])) {
-			$enlaces = $_GET['action'];
-		}else{
-			$enlaces = 'index'
+<?php
+	class MvcController{
+		#llamada a la plantilla
+		public function pagina(){
+			include "views/template.php";
 		}
 
-		//es el momento en que el controlador invoca al modelo llamado "enlacesPaginasModel para que muestre el listado de paginas"
-		$respuesta = Paginas::enlacesPaginasModel($enlaces);
-		include $respuesta;
-	}
-
-	//Registro de usuarios
-	public function registroUsuarioController(){
-		if (isset($_POST["usuarioRegistro"])) {
-			//recibe a traves del metodo POST el name(html) de usuario, password y email, se almacenan los datos en una variable opropiedad de tipo array asociativo con sus respectivas propiedades (usuario, password, email)
-			$datosController = array("usuario"=>$_POST["usuarioRegistro"],"password"=>$_POST["passwordRegistro"],"email"=>$_POST["emailRegistro"])
-			//se le dice al modelo models/crud.php (Datos::registroUsuariosModel), que en el modelo Datos el metodo registroUsuariosModel reciba en sus parametros los valores $datosController y el nombre de la tabla a la cual debe de conectarse (usuarios)
-			$respuesta = Datos::registroUsuarioModel($datosController,"usuarios");
-
-			//se imprime la respuesta en la vista
-			if ($respuesta == "succes") {
-				header(string:"location:index.php?action=ok");
+		//enlaces
+		public function enlacesPaginasController{
+			if(isset($_GET['action'])){
+				$enlaces = $_GET['action'];
 			}else{
-				header(string:"location:index.php");
+				$enlaces = 'index';
+			}
+			//Es el momento en que el controlador invoca el modelo enalcesPaginaModel para que muestre el listaaod de paginas
+			$respuesta = Paginas::enlacesPaginasModel($enlaces);
+			include $respuesta;
+		}
+
+		//registro
+		public function registroUsiaroController(){
+			if(isset($_POST["usuarioRegistro"])){
+				//recibe a traves del metoddo post el name de usuario, passwoer y email se almacenan los datos en una variable o propiedad de tipo array asociativo cpn sus respectivas propiedades
+				$datosController= array("usuario"=>$_POST["usuarioRegistro"],"password"=>$_POST["passwordRegistro"],"email"=>$_POST["emailRegistro"]);
+				//se le dice al modelo model/crud.php (Datos:registroUsuarioModel), en que modelo Datos el metodo registroUsuarioMOdel reciba en sus parmatros los valores $datoaController y el nombre de la tabla a ala cual debe conectarse
+
+				$respuesta =Datos::registroUsuarioModel($datosController,"usuarios");
+				//se imprime la respuesta en la vista
+				if($respuesta=="success"){
+					header(string: "location:index.php?action=ok");
+				}else{
+					header(string: "location:index.php");
+				}
 			}
 		}
-	}
-
-	//ingreso usuarios
-	public function ingresoUsuarioController(){
-		if (isset($_POST["usuarioIngreso"]) {
-			$datosController = array("usuario" => $_POST["usuarioIngreso"], 
-				                     "password" => $_POST["passwordIngreso"]];
-			$respuesta = Datos::ingresoUsuarioModel($datosController, "usuarios");
-
-			//validar la respuesta del modelo para ver si es un usuario correcto
-			if ($respuesta["usuario"]==$_POST["usuarioIngreso"] && $respuesta["password"]==$_POST["passwordIngreso"]) {
-				session_start();
-				$_SESSION["validar"] = true;
-				header("location:index.php?action=usuarios");
-			}else{
-				header("location:index.php?action=fallo");
+		//INgreso usuarios
+		public function ingresoUsuarioController(){
+			if(isset($_POST["usuarioIngreso"])){
+				$datosController= array("usuario"=>$_POST["usuarioIngreso"],"password"=>$_POST["passwordIngreso"]);
+				$respuesta= Datos::ingresoUsuarioModel($datosController,"usuarios");
+				//validar la repsuesta de modelo
+				if($respuesta["usuario"]==$_POST["usuarioIngreso"] $$ $respuesta["password"]== $_POST["passwordIngreso"]){
+					$_SESSION["validar"]=true;
+					header("location:index.php?action=usuarios");
+				}else{
+					header("location:index.php?action=fallo");
+				}
 			}
 		}
-	}
 
-	//vistas de usuarios
-	public function vistaUsuariosController(){
-		$respuesta=Datos::vistaUsuarioModel("usuarios");
-		//utilizar un foreach para iterar un array e imprimir la consulta del modelo
 
-		foreach ($respuesta as $row => $item) {
-			echo '<tr>
-			      <td'.item["usuario"].'</td>
-			      <td'.item["password"].'</td>
-			      <td'.item["email"].'</td>
-			      <td<a href = "index.php?action=usuarios&idEditar='.$item["id"].'"<button>Editar</button></a></td>
-			      <td<a href = "index.php?action=usuarios&idBorrar='.$item["id"].'"<button>Borrar</button></a></td>
-			      </tr>';
+		//Vista de usuarios
+		public function vistaUsuariosController(){
+			$respuesta=Datos::vistaUsuarioModel("usuarios");
+			foreach ($respuesta as $row => $item) {
+				echo '<tr>
+						<td>'.$item["usuario"].'</td>
+						<td>'.$item["password"].'</td>
+						<td>'.$item["email"].'</td>
+						<td><a href=index.php?action=editar&idBorrar='.$item["id"].'<button>Borrar</button></td>
+
+						<td><a href=index.php?action=usuarios&idEditar='.$item["id"].'<button>Editar</button></td>';
+			}
 		}
-	}
-}
 
+		//Editar usuario
+		public function editarUsuarioController(){
+			$datosController=$_GET["id"];
+			$respuesta=Datos::editarUsuarioModel($datosController,"usuarios");
+
+			//Diseñar la estructura de un formulario para que se muestre los datos de la consulta generada en el modelo.
+			echo ('<input type="hidden" value="'.$respuesta["id"].'" name=""idEditar">
+			<input type="text" value="'.$respuesta["usuario"].'" name="usuarioEditar" required>
+			<input type="text" value="'.$respuesta["password"].'" name="passwordEditar" required>
+			<input type="text" value="'.$respuesta["email"].'" name="emailEditar" required>');
+		}
+
+		public function actualizarUsuarioController(){
+			if(isset($_POST["usuarioEditar"])){
+				$datosController=array("id"=>$_POST["idEditar"], "usuario"=>$_POST["usuarioEditar"],"password"=>$_POST["passwordEditar"], "email"=>$_POST["emailEditar"]);
+				$respuesta=Datos::actualizarUsuarioModel($datosController,"usuarios");
+				if($respuesta=="succes"){
+					header("location:index.php?action=cambio");
+				}else{
+					echo("error");
+				}
+			}
+		}
+		public function borrarUsuariosController(){
+			if(isset($_GET["idBorrar"])){
+				$datosController=$_GET["idBorrar"];
+				$respuesta=Datos::borrarUsuarioModel($datosController,"usuarios");
+				if($respuesta=="success"){
+					header("location:index.php?action=usuarios");
+				}
+			}
+		}
+
+		//LISTA DE MODELOS POR DESARROLLAR:
+		/*
+		* 1. registroUsuariosModel
+		* 2. ingresoUsuariosModel
+		* 3. vistaUsuarioModel
+		* 4. editarUsuarioModel
+		* 5. actualizarUsuarioModel
+		* 6. borrarUsuarioModel
+		*/
+	}
 ?>
--- mvc/models/crud.php
+++ mvc/models/crud.php
@@ -69,6 +69,7 @@ class Datos extends Conexion
 		}else{
 			return "error";
 		}
+		$stmt->close();
 	}
 
 	//modelo borrar usuario
