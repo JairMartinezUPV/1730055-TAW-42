b56dfbf2bd704e36c8cc8cda747d44ac02a42b4c
JairMartinezUPV <36892931+JairMartinezUPV@users.noreply.github.com>
2020-05-22 09:07:32 -0500

    update bd

---
+++ Ejercicio_22_May_20/ejercicio.sql
@@ -0,0 +1,132 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 22-05-2020 a las 16:06:50
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ejercicio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estudiante`
+--
+
+CREATE TABLE `estudiante` (
+  `id` int(11) NOT NULL,
+  `cedula` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellidos` varchar(50) NOT NULL,
+  `promedio` int(5) NOT NULL,
+  `edad` int(5) NOT NULL,
+  `fecha` date NOT NULL,
+  `fkUniversidad` int(11) NOT NULL,
+  `fkFacultad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `facultad`
+--
+
+CREATE TABLE `facultad` (
+  `id` int(11) NOT NULL,
+  `nombre` int(11) NOT NULL,
+  `fkUniversidad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `universidad`
+--
+
+CREATE TABLE `universidad` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `estudiante`
+--
+ALTER TABLE `estudiante`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fkFacultad` (`fkFacultad`),
+  ADD KEY `fkUniversidad` (`fkUniversidad`);
+
+--
+-- Indices de la tabla `facultad`
+--
+ALTER TABLE `facultad`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `facultad_ibfk_1` (`fkUniversidad`);
+
+--
+-- Indices de la tabla `universidad`
+--
+ALTER TABLE `universidad`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `estudiante`
+--
+ALTER TABLE `estudiante`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `facultad`
+--
+ALTER TABLE `facultad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `universidad`
+--
+ALTER TABLE `universidad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `estudiante`
+--
+ALTER TABLE `estudiante`
+  ADD CONSTRAINT `estudiante_ibfk_1` FOREIGN KEY (`fkFacultad`) REFERENCES `facultad` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `estudiante_ibfk_2` FOREIGN KEY (`fkUniversidad`) REFERENCES `universidad` (`id`);
+
+--
+-- Filtros para la tabla `facultad`
+--
+ALTER TABLE `facultad`
+  ADD CONSTRAINT `facultad_ibfk_1` FOREIGN KEY (`fkUniversidad`) REFERENCES `universidad` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
