2522d5007eebf8f10a8877226f6e9c30b126788f
JairMartinezUPV <36892931+JairMartinezUPV@users.noreply.github.com>
2020-05-25 08:44:54 -0500

    index

--- Ejercicio_25_May_2020/controller/controller.php
+++ Ejercicio_25_May_2020/controller/controller.php
@@ -1,1 +1,19 @@
-public function ingresoUsuarioC
+<?php
+include_once "models/crud.php";
+include_once "models/crudProd.php";
+
+	class MvcController{
+
+		public function ingresoUsuarioController(){
+			if(isset($_POST["txtUser"]) && isset($_POST["txtPassword"])){
+				$datosController = array("user"=>$_POST["txtUser"],"password"=>$_POST["txtPassword"]);
+				$respuesta = Datos::ingresoUsuarioModel($datosController,"users");
+				if ($respuesta["usuario"]==$_POST["txtUser"] && password_verify($_POST["txtPassword"],$respuesta["contrasena"])) {
+					# code...
+				}
+			}
+		}
+
+	}
+
+?>
