592b25e19e34b13a134744ae77fb759fd09ff61b
JairMartinezUPV <36892931+JairMartinezUPV@users.noreply.github.com>
2020-06-05 09:41:52 -0500

    update

--- Ejercicio_25_May_2020/Models/crud.php
+++ Ejercicio_25_May_2020/Models/crud.php
@@ -139,6 +139,132 @@
 			$stmt->close();
 		}
 
+		public function obtenerCategoryModel($tabla)
+		{
+			$stmt = Conexion::conectar()->prepare("SELECT id_category AS 'id', name_category AS 'categoria' FROM $tabla");
+			$stmt->execute();
+			return $stmt->fetchAll();
+		}
+
+		public function vistaProductsModel($tabla)
+        {
+        	$stmt = Conexion::conectar()->prepare("SELECT p.id_product AS 'id',p.code_producto AS 'codigo', p.name_product AS 'producto',p.date_added AS 'fecha', p.price_product AS 'precio', p.stock AS 'stock', c.name_category AS 'categoria', FROM $tabla p INNER JOIN categories c ON p.id_category = c.id_category");
+        	$stmt->execute();
+        	return $stmt->fetchAll();
+        	$stmt->close();
+        }
+
+        public function insertarProductsModel($datosModel,$tabla){
+        	 $stmt = Conexion::conectar()->prepare("INSERT INTO $tabla (code_producto,name_product,price_product,stock,id_category) VALUES (:codigo,:nombre,:precio,:stock,:categoria)");
+        	 $stmt->bindParam(":codigo",$datosModel["codigo"],PDO::PARAM_STR);
+        	 $stmt->bindParam(":nombre",$datosModel["nombre"],PDO::PARAM_STR);
+        	 $stmt->bindParam(":precio",$datosModel["precio"],PDO::PARAM_STR);
+        	 $stmt->bindParam(":id",$datosModel["id"],PDO::PARAM_INT);
+        	 if ($stmt->execute()) {
+        	 	return "success";
+        	 }else{
+        	 	return "error";
+        	 }
+        	 $stmt->close();
+        }
+
+        public function editarProductsModel($datosModel,$tabla)
+        {
+        	$stmt = Conexion::conectar()->prepare("SELECT id_product AS 'id', code_producto AS 'codigo', name_product AS 'nombre', price_product AS 'precio', stock FROM $tabla WHERE id_product=:id");
+        	$stmt->bindParam(":id",$datosModel,PDO::PARAM_INT);
+        	$stmt->execute();
+        	return $stmt->fetch();
+        	$stmt->close();
+        }
+
+        public function pushProductsModel($datosModel,$tabla)
+        {
+        	$stmt = Conexion::conectar()->prepare("UPDATE $tabla SET stock = stock + :stock WHERE id_product = :id");
+        	$stmt -> bindParam(":stock", $datosModel["stock"], PDO::PARAM_INT);
+        	$stmt -> bindParam(":id", $datosModel["id"], PDO::PARAM_INT);
+        	if ($stmt->execute()) {
+        		return "success";
+        	}else{
+        		return "error";
+        	}
+        	$stmt->close();
+        }
+
+         public function pullProductsModel($datosModel,$tabla)
+        {
+        	$stmt = Conexion::conectar()->prepare("UPDATE $tabla SET stock = stock - :stock WHERE id_product = :id AND stock >= :stock");
+        	$stmt -> bindParam(":stock", $datosModel["stock"], PDO::PARAM_INT);
+        	$stmt -> bindParam(":id", $datosModel["id"], PDO::PARAM_INT);
+        	if ($stmt->execute()) {
+        		return "success";
+        	}else{
+        		return "error";
+        	}
+        	$stmt->close();
+        }
+
+        public function actualizarProductsModel($datosModel,$tabla){
+			$stmt = Conexion::conectar()->prepare("UPDATE $tabla SET code_producto = :codigo, name_product = :nombre, price_product = :precio,id_category=:categoria, stock = :stock WHERE id_product = :id");
+			
+			$stmt->bindParam(":codigo",$datosModel["codigo"],PDO::PARAM_STR);
+			$stmt->bindParam(":nombre",$datosModel["nombre"],PDO::PARAM_STR);
+			$stmt->bindParam(":precio",$datosModel["precio"],PDO::PARAM_INT);
+			$stmt->bindParam(":categoria",$datosModel["categoria"],PDO::PARAM_INT);
+			$stmt->bindParam(":stock",$datosModel["stock"],PDO::PARAM_INT);
+			$stmt->bindParam(":id",$datosModel["id"],PDO::PARAM_STR);
+
+			if ($stmt->execute()) {
+				return "success";
+			}else{
+				return "error";
+			}
+			$stmt->close();
+		}
+
+		public function eliminarProductsModel($datosModel,$tabla){
+			$stmt = Conexion::conectar()->prepare("DELETE FROM $tabla WHERE id_product = :id");
+			$stmt->bindParam(":id",$datosModel,PDO::PARAM_INT);
+			if ($stmt->execute()) {
+				return "success";
+			}else{
+				return "error";
+			}
+			$stmt->close();
+		}
+
+		public function ultimoProductsModel($tabla)
+		{
+			$stmt = Conexion::conectar()->prepare("SELECT id_product AS 'id' FROM $tabla ORDER BY id_product DESC LIMIT 1");
+			$stmt->execute();
+			return $stmt->fetch();
+			$stmt->close();
+		}
+
+		public function vistaHistoriaModel($tabla)
+		{
+			$stmt = Conexion::conectar()->prepare("SELECT CONCAT(u.firstname,':',u.user_name) AS 'usuario', p.name_product AS 'producto',h.date AS 'fecha', h.reference AS 'referencia', h.note AS 'nota', h.quantity AS 'cantidad', FROM $tabla h INNER JOIN products p ON h.id_producto = p.id_producto INNER JOIN users u ON h.user_id = u.user_id");
+			$stmt->execute();
+			return $stmt
+		}
+
+		 public function insertarHistorialModel($datosModel,$tabla){
+        	 $stmt = Conexion::conectar()->prepare("INSERT INTO $tabla (user_id,quantity,id_producto,note,reference) VALUES (:user,:cantidad,:producto,:note,:reference)");
+        	 $stmt->bindParam(":user",$datosModel["user"],PDO::PARAM_INT);
+        	 $stmt->bindParam(":cantidad",$datosModel["cantidad"],PDO::PARAM_INT);
+        	 $stmt->bindParam(":producto",$datosModel["producto"],PDO::PARAM_INT);
+        	 $stmt->bindParam(":note",$datosModel["note"],PDO::PARAM_STR);
+        	 $stmt->bindParam(":reference",$datosModel["reference"],PDO::PARAM_STR);
+        	 if ($stmt->execute()) {
+        	 	return "success";
+        	 }else{
+        	 	return "error";
+        	 }
+        	 $stmt->close();
+        }
+
+		
+	}
+
 		
 	}
 
